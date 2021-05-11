66a1ee5323900329b00d7c27482c77d8f29433fa
Kriss Gardner <krissrobert12@gmail.com>
2021-05-12 02:04:14 +0300

    Create game app

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "begameshopapp.users.apps.UsersConfig",
+    "begameshopapp.games.apps.GamesConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
