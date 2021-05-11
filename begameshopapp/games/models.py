8887f6f628ddc43ebf216384a3bee153dd50e84b
Kriss Gardner <krissrobert12@gmail.com>
2021-05-12 02:28:08 +0300

    Add description field to game model

--- begameshopapp/games/models.py
+++ begameshopapp/games/models.py
@@ -8,6 +8,8 @@ class Game(models.Model):
     seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='products')
     owners = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='owned_games')
 
+    description = models.CharField(blank=True, max_length=512)
+
 
 class Category(models.Model):
     name = models.CharField(blank=False, max_length=64)
