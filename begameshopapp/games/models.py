3d64147cf54745012b06b859ea4b85928df9d20d
Kriss Gardner <krissrobert12@gmail.com>
2021-05-12 02:21:14 +0300

    Create and migrate game model

--- begameshopapp/games/models.py
+++ begameshopapp/games/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    key = models.CharField(blank=False, max_length=64)
+    seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='products')
+    owners = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='owned_games')
