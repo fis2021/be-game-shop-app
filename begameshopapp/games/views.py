d91d36df9aaaf2f8db5c043885a44d0c7ca9a05d
Kriss Gardner <krissrobert12@gmail.com>
2021-05-12 23:56:08 +0300

    Create game viewset

--- begameshopapp/games/views.py
+++ begameshopapp/games/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import *
 
 # Create your views here.
+
+
+class GameViewSet(ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
