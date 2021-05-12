f2dacda8502e010ea83be2b2f0ba1c1b5e41d0ef
Kriss Gardner <krissrobert12@gmail.com>
2021-05-13 00:21:29 +0300

    Register viewsets

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from begameshopapp.users.api.views import UserViewSet
+from begameshopapp.games.views import *
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,7 +10,9 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
-
+router.register("games", GameViewSet)
+router.register("categories", CategoryViewSet)
+router.register("reviews", ReviewViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
