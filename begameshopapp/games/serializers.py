389948c4b933b0627cdd13d89dcb6f939abff17d
Kriss Gardner <krissrobert12@gmail.com>
2021-05-12 23:44:33 +0300

    Category & review serializers

--- begameshopapp/games/serializers.py
+++ begameshopapp/games/serializers.py
@@ -6,3 +6,15 @@ class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = '__all__'
