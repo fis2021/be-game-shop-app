988b0623b380cf2a9585de19113614341d1f5983
Kriss Gardner <krissrobert12@gmail.com>
2021-05-16 17:36:41 +0300

    Add name field to game model

--- begameshopapp/games/models.py
+++ begameshopapp/games/models.py
@@ -5,6 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Game(models.Model):
+    name = models.CharField(blank=False, max_length=64)
     key = models.CharField(blank=False, max_length=64)
     seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='products')
     owners = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='owned_games')
